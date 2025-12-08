687439bbf0caab04840e6f65e61f85efaa559932
lhwdev <lhwdev6@outlook.com>
2025-12-09 00:03:10 +0900

    fixed where minRadius for track build included gradient (TODO: add support for tilt)

---
+++ modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/compat/BezierConnectionCompat.kt
@@ -0,0 +1,9 @@
+package com.lhwdev.minecraft.railx.compat
+
+import com.simibubi.create.content.trains.track.BezierConnection
+
+
+@Suppress("UnusedReceiverParameter", "unused")
+fun BezierConnection.copiedFrom(from: BezierConnection) {
+	// TODO: copies slab info from SnR
+}
