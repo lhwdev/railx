e21a4c9b2be9e84a5b4f7dac7fd170856c00a508
lhwdev <lhwdev6@outlook.com>
2025-12-24 08:17:44 +0900

    make common.preciseOverlay more precise; avoid some cases where Create placement is keep showing

---
+++ modules/forge/src/main/java/com/lhwdev/minecraft/railx/mixin/common/TrackPlacementMixin.java
@@ -0,0 +1,30 @@
+package com.lhwdev.minecraft.railx.mixin.common;
+
+import com.lhwdev.minecraft.railx.common.CreateTrackPlacement;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.simibubi.create.content.trains.track.TrackPlacement;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(TrackPlacement.class)
+public class TrackPlacementMixin {
+	@Inject(method = "clientTick", at = @At("HEAD"), remap = false)
+	private static void beforeClientTick(CallbackInfo ci) {
+		CreateTrackPlacement.INSTANCE.setLastOverlay(null);
+	}
+	
+	@ModifyExpressionValue(method = "clientTick", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content" +
+		"/trains/track/TrackPlacement;tryConnect(Lnet/minecraft/world/level/Level;" +
+		"Lnet/minecraft/world/entity/player/Player;" +
+		"Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;" +
+		"Lnet/minecraft/world/item/ItemStack;ZZ)" +
+		"Lcom/simibubi/create/content/trains/track/TrackPlacement$PlacementInfo;", ordinal = 0, remap = false),
+		remap = false)
+	private static TrackPlacement.PlacementInfo mapPlacementInfo(TrackPlacement.PlacementInfo original) {
+		CreateTrackPlacement.INSTANCE.setLastOverlay(original);
+		return original;
+	}
+}
---
+++ modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/common/CreateTrackPlacement.kt
@@ -0,0 +1,8 @@
+package com.lhwdev.minecraft.railx.common
+
+import com.simibubi.create.content.trains.track.TrackPlacement
+
+
+object CreateTrackPlacement {
+	var lastOverlay: TrackPlacement.PlacementInfo? = null
+}
