2926ae1257cfcd1931dda01ef14672666d71e75b
lhwdev <lhwdev6@outlook.com>
2026-04-24 16:43:14 +0900

    [middleTrack] use WorldAttached to reduce unknown bugs (IDK if they happen but)

--- modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/middleTrack/visual/MiddleTrackVisuals.kt
+++ modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/middleTrack/visual/MiddleTrackVisuals.kt
@@ -4,6 +4,7 @@ import com.lhwdev.minecraft.railx.middleTrack.ConnectionMiddleState
 import com.lhwdev.minecraft.railx.middleTrack.GlobalConnections
 import com.lhwdev.minecraft.railx.middleTrack.MiddleTrackClient
 import dev.engine_room.flywheel.api.visualization.VisualizationManager
+import net.createmod.catnip.data.WorldAttached
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.LevelAccessor
@@ -13,37 +14,50 @@ import net.neoforged.api.distmarker.OnlyIn
 
 @OnlyIn(Dist.CLIENT)
 object MiddleTrackVisuals {
-	private var previousLoadedTracks = emptySet<BlockPos>()
-	private var previousMiddles = emptySet<ConnectionMiddleState>()
+	private class State {
+		var previousLoadedTracks = emptySet<BlockPos>()
+		var previousMiddles = emptySet<ConnectionMiddleState>()
+		
+		fun clear() {
+			previousLoadedTracks = emptySet()
+			previousMiddles = emptySet()
+		}
+	}
+	
+	private val state = WorldAttached { State() }
 	
 	fun tick(level: Level) {
 		if(!VisualizationManager.supportsVisualization(level)) {
-			previousLoadedTracks = emptySet()
-			previousMiddles = emptySet()
+			state[level].clear()
 			return
 		}
 		val manager = MiddleTrackVisualManager.of(level) ?: return
 		
+		val state = state[level]
 		val connections = GlobalConnections[level]
 		val middles = connections.toSet()
-		for(added in middles - previousMiddles) {
+		for(added in middles - state.previousMiddles) {
 			manager.queueAdd(added)
 			// manager.queueUpdate(added)
 		}
-		for(removed in previousMiddles - middles) manager.queueRemove(removed)
-		previousMiddles = middles
+		for(removed in state.previousMiddles - middles) manager.queueRemove(removed)
+		state.previousMiddles = middles
 		
-		val loadedTracks = MiddleTrackClient.LoadedTracks
-		for(added in loadedTracks - previousLoadedTracks) connections.getAll(added).forEach(manager::queueUpdate)
-		for(removed in previousLoadedTracks - loadedTracks) connections.getAll(removed).forEach(manager::queueUpdate)
-		previousLoadedTracks = loadedTracks
+		val loadedTracks = MiddleTrackClient.LoadedTracks[level]
+		for(added in loadedTracks - state.previousLoadedTracks)
+			connections.getAll(added).forEach(manager::queueUpdate)
+		for(removed in state.previousLoadedTracks - loadedTracks)
+			connections.getAll(removed).forEach(manager::queueUpdate)
+		state.previousLoadedTracks = loadedTracks
 	}
 	
 	fun tickDisable(level: LevelAccessor) {
 		if(!VisualizationManager.supportsVisualization(level)) return
 		val manager = MiddleTrackVisualManager.of(level) ?: return
-		for(middle in previousMiddles) manager.queueRemove(middle)
-		previousLoadedTracks = emptySet()
-		previousMiddles = emptySet()
+		val state = state[level]
+		
+		for(middle in state.previousMiddles) manager.queueRemove(middle)
+		state.previousLoadedTracks = emptySet()
+		state.previousMiddles = emptySet()
 	}
 }
