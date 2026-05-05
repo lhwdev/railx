70c3185b3bab7efd11626534470f69b112de3736
lhwdev <lhwdev6@outlook.com>
2026-05-05 22:43:51 +0900

    [compat/trainMap] remove Xaero's World Map support
    - originally planned to add more features than Create integration provides
    - but forgave to do so

--- modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/trainMap/GuiMapInit.kt
+++
@@ -1,9 +0,0 @@
-package com.lhwdev.minecraft.railx.trainMap
-
-import xaero.map.gui.GuiMap
-
-object GuiMapInit {
-	fun init(map: GuiMap) {
-		// val container = TrainMapLayers()
-	}
-}
--- modules/forge/src/main/kotlin/com/lhwdev/minecraft/railx/trainMap/common.kt
+++
@@ -1,4 +0,0 @@
-package com.lhwdev.minecraft.railx.trainMap
-
-
-data class MapPos(val x: Int, val z: Int)
